7c8fce5ce05b0085223d5f34f0653170ac3cd5d7
konabuta <keonabut@microsoft.com>
2022-07-01 02:43:14 +0000

    ja localize init

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'ja'],
   },
 
   presets: [
@@ -63,6 +63,10 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
+          {
+            type: 'localeDropdown',
+            position: 'left',
+          },
           {
             type: 'doc',
             docId: 'intro',
--- my-website/src/components/HomepageFeatures/index.tsx
+++ my-website/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Tips & Practices',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Collection of Tips & Practices of Azure Machine Learning.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'FastTrack for Azure - Sessino materials',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Session materials of FastTrack for Azure Live sessions.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'References',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Useful documentations, tools and sample codes.
       </>
     ),
   },
